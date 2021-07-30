e6c85f7e5a8c7819b7b7afd1a449e810634e195c
Dan Gee <dangee1705@gmail.com>
2021-07-30 16:29:27 +0100

    added comments to networking code

--- src/holworthy/chess/networking/Client.java
+++ src/holworthy/chess/networking/Client.java
@@ -1,5 +1,23 @@
 package holworthy.chess.networking;
 
 public class Client {
+	// server host
+	// server port
+	// chess
+	// colour
+
+	// send move
+	// recv move
 	
+	// 0(String name)
+	// 1(Move move)
+
+	// onConnect
+	// onConnectError
+	// onRecieveColour
+	// onRecieveOpponentName
+	// onGameStart
+	// onRecieveInvalidMove
+	// onRecieveMove
+	// onGameEnd
 }
--- src/holworthy/chess/networking/Server.java
+++ src/holworthy/chess/networking/Server.java
@@ -1,5 +1,25 @@
 package holworthy.chess.networking;
 
 public class Server {
-	
+	// port
+	// chess
+	// white client
+	// black client
+
+	// send colour
+	// send name?
+	// recv move + validate + send to both
+	// win condition
+
+	// send clients colour 0(int colour)
+	// send oponent name 1(String name)
+	// invalid move 2 
+	// valid move 3(Move move)
+	// win/loss 4(boolean win)
+
+	// onStart
+	// onStartError
+	// onGameStart
+	// onRecieveMove
+	// onGameEnd
 }
